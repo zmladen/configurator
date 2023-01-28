084b73fefd50f67b52cc43cabae9f37f76c05dde
Mladen Zec <Mladen.Zec@buehlermotor.com>
2023-01-28 15:18:54 +0100

    new user

--- frontend/src/components/Users/components/User/index.jsx
+++ frontend/src/components/Users/components/User/index.jsx
@@ -5,7 +5,11 @@ import Container from "../../../Container";
 import Button from "../../../Button";
 import styles from "./Styles/User.module.css";
 import { useUser } from "../../../../context/userContext";
-import { UserIcon } from "@heroicons/react/24/solid";
+import { EnvelopeIcon, PhoneIcon, DevicePhoneMobileIcon } from "@heroicons/react/24/solid";
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 1.5H8.25A2.25 2.25 0 006 3.75v16.5a2.25 2.25 0 002.25 2.25h7.5A2.25 2.25 0 0018 20.25V3.75a2.25 2.25 0 00-2.25-2.25H13.5m-3 0V3h3V1.5m-3 0h3m-3 18.75h3" />
+</svg>;
 
 function User(props) {
   const navigate = useNavigate();
@@ -20,40 +24,28 @@ function User(props) {
   return (
     <Container>
       <div className={styles.User}>
-        <div className={styles.Image}>
-          <UserIcon />
-        </div>
-        <div className={styles.Infos}>
+        <header className={styles.header}>
+          <img src={"/images/maleProfile.png"} className={styles.profileImg} alt="profile" />
+          <h1 className={styles.name}>{`${user?.firstname}, ${user?.lastname}`}</h1>
+        </header>
+        <section className={styles.about}>
+          <h2 className={styles.sectionHeader}>About</h2>
+          <p>I am a software developer with 5 years of experience.</p>
+        </section>
+        <section className={styles.contact}>
+          <h2 className={styles.sectionHeader}>Contact</h2>
           <p>
-            <strong>First Name:</strong> {user?.firstname}
+            <EnvelopeIcon /> {user?.email}
           </p>
           <p>
-            <strong>Last Name:</strong> {user?.lastname}
+            <PhoneIcon />: {user?.telephone?.office}
           </p>
           <p>
-            <strong>Username:</strong> {user?.username}
+            <DevicePhoneMobileIcon />: {user?.telephone?.mobile}
           </p>
-          <p>
-            <strong>Email:</strong> {user?.email}
-          </p>
-          <p>
-            <strong>Admin:</strong> {user?.admin ? "Yes" : "No"}
-          </p>
-          <p>
-            <strong>Status:</strong> {user?.status}
-          </p>
-          <p>
-            <strong>Created:</strong> {user?.created}
-          </p>
-          <p>
-            <strong>Mobile Phone:</strong> {user?.telephone?.mobile}
-          </p>
-          <p>
-            <strong>Office Phone:</strong> {user?.telephone?.office}
-          </p>
-        </div>
+        </section>
       </div>
-      <br />
+
       <Button className="btn btn-dark btn-lg br-25 pt-10 pb-10 pl-20 pr-20" type="button" onClick={() => navigate(-1)}>
         Go Back
       </Button>
